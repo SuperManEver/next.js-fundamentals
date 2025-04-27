b25588d4433e6c65a983b489864b579aef62415a
Nik Luparev <nikita.luparev@gmail.com>
2025-04-27 12:49:49 +0500

    main pages stub

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MarketingRoot() {
+  return <div>MarketingRoot</div>
+}
+
+export default MarketingRoot
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NewIssuePage() {
+  return <div>NewIssuePage</div>
+}
+
+export default NewIssuePage
